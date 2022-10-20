c166ae72986db81076f78f497c30d72048633c37
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-20 15:48:34 -0300

    estrutura while

---
+++ Capgemini/EstruturaWhile/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int totalAlunos = 1;
+        Scanner leitorScanner = new Scanner(System.in);
+
+        while (totalAlunos > 0) {
+
+            System.out.println("Digite o nome do aluno: ");
+            String nomeAluno = leitorScanner.nextLine();
+
+            System.out.println("Digite a idade do aluno: ");
+            int idadeAluno = leitorScanner.nextInt();
+
+            System.out.println("O nome do aluno é " + nomeAluno + " e sua idade é " + idadeAluno + " anos.");
+
+           totalAlunos = totalAlunos - 1;
+        }
+
+    }
+}
