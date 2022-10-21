e336d1f09cb685436c9c057c3153cf37bad7e427
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-21 15:15:21 -0300

    heranca aula 3

--- Capgemini/Heranca/src/Main.java
+++ Capgemini/Heranca/src/Main.java
@@ -1,11 +1,20 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
         Vendedor v = new Vendedor();
-        v.
+        v.setNome("Márcio");
+        v.setSalario(1000.0f);
+        v.setCpf("23723424");
+        v.setDataNascimento(new Date());
+        v.setComissaoPorItem(10.0f);
+        v.setTotalItensVendidos(10);
+
+        System.out.println("O salário do vendedor é: " + v.calcularSalario());
 
         Motorista m = new Motorista();
-
+    
 
     }
 }
--- Capgemini/Heranca/src/Vendedor.java
+++ Capgemini/Heranca/src/Vendedor.java
@@ -9,4 +9,23 @@ public class Vendedor extends Funcionario {
         super();
     }
 
+    public float calcularSalario() {
+        return super.getSalario() + (this.comissaoPorItem * totalItensVendidos);
+    }
+
+    public int getTotalItensVendidos() {
+        return totalItensVendidos;
+    }
+
+    public void setTotalItensVendidos(int totalItensVendidos) {
+        this.totalItensVendidos = totalItensVendidos;
+    }
+
+    public float getComissaoPorItem() {
+        return comissaoPorItem;
+    }
+
+    public void setComissaoPorItem(float comissaoPorItem) {
+        this.comissaoPorItem = comissaoPorItem;
+    }
 }
