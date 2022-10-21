f4ad9e0990dbd3ff77e8cf72dd4edbbb0e12191c
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-21 15:03:27 -0300

    heranca aula 2

--- Capgemini/Heranca/src/Main.java
+++ Capgemini/Heranca/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Vendedor v = new Vendedor();
-        v.setNome(nome);
+        v.
+
         Motorista m = new Motorista();
 
 
--- Capgemini/Heranca/src/Vendedor.java
+++ Capgemini/Heranca/src/Vendedor.java
@@ -1,45 +1,12 @@
 import java.util.Date;
 
-public class Vendedor {
+public class Vendedor extends Funcionario {
 
-    private String nome;
-    private String cpf;
-    private Date dataNascimento;
-    private float salario;
+    private int totalItensVendidos;
+    private float comissaoPorItem;
 
     public Vendedor() {
-
+        super();
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public float getSalario() {
-        return salario;
-    }
-
-    public void setSalario(float salario) {
-        this.salario = salario;
-    }
 }
