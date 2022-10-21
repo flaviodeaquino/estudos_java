50319af4395fe5f51e8ffe4e38d5a370989f20b5
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-21 14:49:08 -0300

    introducao heranca

---
+++ Capgemini/Heranca/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Vendedor v = new Vendedor();
+        v.setNome(nome);
+        Motorista m = new Motorista();
+
+
+    }
+}
---
+++ Capgemini/Heranca/src/Vendedor.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class Vendedor {
+
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private float salario;
+
+    public Vendedor() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
