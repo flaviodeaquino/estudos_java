a6489b6fcdc0a4dd11fa4d777a516deda09773de
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-26 11:21:42 -0300

    lista 1 exercicio 8

---
+++ Capgemini/Exercicios/Exercicio08/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor em Real: ");
+        float real = scan.nextFloat();
+        System.out.println("Digite a cotação do dólar (1 dolár = x reais): ");
+        float cotacao = scan.nextFloat();
+        float dolar = real / cotacao;
+        System.out.println(real + " reais é equivalente à " + dolar + " doláres." );
+        
+    }
+}
