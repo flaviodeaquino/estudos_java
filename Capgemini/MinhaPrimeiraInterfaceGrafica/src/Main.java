6d5f78623c2092ffbb893318508bd2b5d14889fa
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-11-09 18:15:23 -0300

    estudo interface grafica - testes no intelliJ

--- Capgemini/MinhaPrimeiraInterfaceGrafica/src/Main.java
+++ Capgemini/MinhaPrimeiraInterfaceGrafica/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("PrimeiraTela");
+        frame.setContentPane(new PrimeiraTela().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- Capgemini/MinhaPrimeiraInterfaceGrafica/src/PrimeiraTela.java
+++ Capgemini/MinhaPrimeiraInterfaceGrafica/src/PrimeiraTela.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class PrimeiraTela {
+    private JButton button_msg;
+    JPanel panelMain;
+    private JTextField textField1;
+
+    public PrimeiraTela() {
+        button_msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Hello World!");
+            }
+        });
+    }
 }
