d64b2fd0c0e1fc57a187aaf87e09da41df982281
flaviodeaquino <flavio.deaquinof@gmail.com>
2023-01-06 12:29:45 -0300

    Criar método para o grid de tarefas - 2

--- Capgemini/NETBEANS/TodoappNetBeans/app/src/main/java/util/TaskTableModel.java
+++ Capgemini/NETBEANS/TodoappNetBeans/app/src/main/java/util/TaskTableModel.java
@@ -4,7 +4,10 @@
  */
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
+import model.Task;
 
 /**
  *
@@ -12,19 +15,22 @@ import javax.swing.table.AbstractTableModel;
  */
 public class TaskTableModel extends AbstractTableModel {
 
+    String[] columns = {"Nome", "Descrição", "Prazo", "Tarefa Concluída", "Editar", "Excluir"}; 
+    List<Task> tasks = new ArrayList();
+    
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
     }
 
     @Override
     public Object getValueAt(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
 }
