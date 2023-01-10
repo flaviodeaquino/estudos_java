965d316a95866cde294f43f8020a9165ca8822cc
flaviodeaquino <flavio.deaquinof@gmail.com>
2023-01-10 16:35:24 -0300

    Finalizando as funcionalidade básicas - aula 17G

--- Capgemini/NETBEANS/TodoappNetBeans/app/src/main/java/view/MainScreen.java
+++ Capgemini/NETBEANS/TodoappNetBeans/app/src/main/java/view/MainScreen.java
@@ -387,6 +387,15 @@ public class MainScreen extends javax.swing.JFrame {
         
         taskDialogScreen.setVisible(true);
         
+        taskDialogScreen.addWindowListener(new WindowAdapter() {
+            public void windowClosed(WindowEvent e) {
+                int projectIndex = jListProjects.getSelectedIndex();
+                Project project = (Project) projectsModel.get(projectIndex);
+                loadTasks(project.getId());
+            }
+                   
+        });
+        
     }//GEN-LAST:event_jLabelTasksAddMouseClicked
 
     private void jTableTasksMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableTasksMouseClicked
