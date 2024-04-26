71625a4b1fe559e5d16368442c65ca6bfc6339bf
flaviodeaquino <flavio.deaquinof@gmail.com>
2024-04-26 12:35:55 -0300

    aula 98 - lançando exceção checked

--- DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -2,15 +2,28 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest02 {
     public static void main(String[] args) {
-        divisao(1, 0);
-    }
-
-    private static int divisao(int a, int b) {
         try {
-            return a/b;
+            divisao(1, 0);
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
-        return 0;
+        System.out.println("Código finalizado.");
+
+    }
+
+    /**
+     *
+     * @param a
+     * @param b não pode ser 0 (zero)
+     * @return
+     * @throws IllegalArgumentException caso b seja 0 (zero)
+     */
+
+    private static int divisao(int a, int b) {
+        if (b==0) {
+            throw new IllegalArgumentException(("Argumento inválido. Não é possível realizar um divisão por 0 (zero)."));
+        }
+
+        return a/b;
     }
 }
